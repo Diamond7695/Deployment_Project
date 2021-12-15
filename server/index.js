de9724dec8d7b869008792db4b06489d01a8b0cd
Diamond Toliver <diamondtoliver@Diamonds-MacBook-Pro.local>
2021-12-15 16:16:56 -0500

    Adding css

--- server/index.js
+++ server/index.js
@@ -3,10 +3,14 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.get('/js',function(req,res){
+app.get('/',function(req,res){
     res.sendFile(path.join(__dirname, '../index.html'))
 })
-app.use('/js', express.static(path.join(__dirname, '../index.js')))
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.js'))
+  })
+
+
 
 
 const port = process.env.PORT || 4005
