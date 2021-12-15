87f2f11ef6cbf1a08478812826e720960fb69f22
Diamond Toliver <diamondtoliver@Diamonds-MacBook-Pro.local>
2021-12-15 16:12:46 -0500

    New changes

--- server/index.js
+++ server/index.js
@@ -6,7 +6,10 @@ const app = express()
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname, '../index.html'))
 })
+app.use('/js', express.static(path.join(__dirname, 'public/index.js')))
+
 
 const port = process.env.PORT || 4005
 
 app.listen(port, ()=>{console.log(`Port works ${port}`)})
+
